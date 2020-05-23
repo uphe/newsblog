083a63985061fbecc80ff1461cff3eab1b76e117
贺志营 <923621813@qq.com>
2020-05-23 10:20:48 +0800

    实现发布文章的标签和分类

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -1,180 +1,199 @@
 $(function () {
-    // 需求：
-    // 1. 点击发布文章 弹出发布模块
-    // 2. 点击发布文章中的关闭按钮 关闭发布文章模块
-    // 3. 添加文章标签
-    // 获取元素
-    var postMain = $(".csdn-post-article-modal"); // 整个发布模块
-
-    // 1. 弹出发布模块
-    var showPostMain = $(".show-post-article-btn");
-    showPostMain.on("click", function () {
-        $(postMain).fadeIn();
+    // 单击按钮弹出模态框
+    $(".publish").on("click", function () {
+        $(".modal").show();
+        $(".mask").show()
     })
-
-    // 2 点击发布文章中的关闭按钮 关闭发布文章模块
-    var hidePostMainBtn = $(".modal-publish button");
-    hidePostMainBtn.on("click", function () {
-        $(this).parents(".csdn-post-article-modal").fadeOut();
+    // 点击X关闭模态框
+    $(".modal-content-head").children("button").on("click", function () {
+        $(".modal").hide();
+        $(".mask").hide()
     })
+    // 添加文章标签弹出模态框
+    $(".mark-selection .add").on("click", function () {
+        $(".mark-selection-box").show()
+    })
+    $(".mark-selection-box .close").on("click", function () {
+        $(".mark-selection-box").hide()
+    })
+    //添加文章标签
+    // 在输入框中添加标签
 
-    // 所有生成的文章标签列 
-    var tagsList = $(".mark-section-title > div:first").children(".tags-list");
-    console.log(tagsList);
-    tagsListClickEvent(tagsList);
-    // 3. 添加文章标签
-    var addArticleTagBtn = $(".article-tag .mark-section-title .add-article-tag");
-    addArticleTagBtn.on("click", function () {
-        tagsListClickEvent(tagsList);
-        // (1) 点击添加文章标签 显示和隐藏标签框
-        $(".mark-section div.mark-section-box").show();
+    function addtag() {
+        // 新建标签
+        $(".mark-selection-box-input input").on("keyup", function (e) {
+            if ($(this).val() !== "" && e.keyCode == 13) {
+                var content = $('<button class="add-tag add-tag-tags"><span>' + $(this).val() + '</span><i class="add-tag-close">x<i></button>')
+                $(".mark-selection").prepend(content);
+                // console.log(content.attr("data-index"));
+                $(this).val("");
+                tagChanged();
+                // 为新添加的标签X绑定点击事件
+                removeTag($(".add-tag-close"),tagChanged);
 
-        // (2) 点击x号 隐藏标签框 
-        $(".mark-section-box-selectTags .icon-close").on("click", function () {
-            $(this).parents(".mark-section-box").hide();
-        })
-
-        // 获取添加文章标签的区域
-        var domArticleTags = $(this).siblings("div").get(0); // jquery对象转化为dom对象
-        // (3) 用户自己添加文章标签
-        var articleTagText = $(".mark-section-box-search input").val();
-
-        // 表单按键事件
-        $(".mark-section-box-search input").on("keyup", function (e) {
-            // (3.1)内容不空
-            if ($(this).val() !== "") {
-                // (3.2)按下的不是enter键 就取表单值
-                if (e.keyCode !== 13) {
-                    articleTagText = $(this).val();
-                    return;
-                } else {
-                    tagsListClickEvent(tagsList);
-                    // (3.3)如果是enter键就创建元素 获取值 并添加元素
-                    var domTags = '<div class="mark-section-tag-box tags-list"><span>' + articleTagText + '</span><i class="icon icon-remove">x</i></div>'
-                    tagsList = $(".mark-section-title > div:first").children(".tags-list");
-                    // 文章到达5个就不能再添加了
-                    if (tagsList.length >= 0 && tagsList.length <= 4) {
-                        domArticleTags.insertAdjacentHTML("afterbegin", domTags);
-                        tagsList = $(".mark-section-title > div:first").children(".tags-list");
-                        $(".mark-section-box .mark-section-box-selectTags span em").text(5 - tagsList.length);
-                        // console.log(tagsList);
-                        // console.log("文章标签：" + tagsList.length);
-
-                        // (3.4) 添加新的标签后 重新绑定x号点击事件
-                        tagsListClickEvent(tagsList);
-                    } else if (tagsList.length = 5) {
-                        $(".mark-section-box .mark-section-box-selectTags span").html("<span style = 'color:red;'>最多只能添加5个标签！</span>");
-                    }
-                    tagsListClickEvent(tagsList);
-                }
             }
-            // 最后键入标签成功后 表单值清空
-            $(this).val("");
-            tagsListClickEvent(tagsList);
         })
-    })
+        // 显示已存在标签
+        // 1. 排他思想 添加current类名
+        $('.tags-items li').click(function () {
+            $(this).addClass('isactive').siblings().removeClass('isactive');
+            // 2. 点击的同时得到当前li的索引号 方便显示内容的匹配
+            var index = $(this).index();
+            // 3. 显示对应内容
+            $('.tag-items-content div').eq(index).show().siblings().hide();
+        })
 
-    // 判断文章标签的个数
-    function tagsCount(tagsList) {
-        if (tagsList.length >= 0 && tagsList.length <= 4) {
-            $(".mark-section-box .mark-section-box-selectTags span em").text(5 - tagsList.length);
-            tagsList = $(".mark-section-title > div:first").children(".tags-list");
-        } else if (tagsList.length = 5) {
-            // alert("只能添加5个标签哦");
-            $(".mark-section-box .mark-section-box-selectTags span").html("<span style = 'color:red;'>最多只能添加5个标签！</span>");
+        // 添加已存在标签
+        $(".tag-items-content div li").unbind('click').click(function () {
+            var content = $('<button class="add-tag add-tag-tags"><span>' + $(this).text() + '</span><i class="add-tag-close">x<i></button>')
+            $(".mark-selection").prepend(content);
+            tagChanged();
+            removeTag($(".add-tag-close"), tagChanged);
+        })
+    }
+
+    function removeTag(element, callback) {
+        // 点击x移除
+        element.unbind('click').click(function (e) {
+            $(".tag-options-list span").each(function() {
+                console.log($(this).text());
+                console.log($(e.target).siblings("span").text())
+                if($(this).text()==$(e.target).siblings("span").text()) {
+                    $(this).siblings(".checkbox").prop("checked",false)
+                }
+            })
+            $(this).parent().remove();
+            callback();
+        })
+
+    }
+    function tagChanged() {
+        if ($(".add-tag-tags").length == 5) {
+            // alert("hello")
+            // 解绑键入事件
+            $(".mark-selection-box-input input").off();
+            // 隐藏添加标签按钮
+            $(".mark-selection .add").hide();
+            $(".tag-num").hide();
+            $(".max-tag-num").show();
+        } else {
+            $(".mark-selection .add").show();
+            $(".tag-num").show();
+            $(".max-tag-num").hide();
+            addtag();
+            //  alert(5 - $(".add-tag").length)
+            var num = 5 - $(".add-tag-tags").length;
+            $(".mark-selection-box-selectTag .tag-num").text('还可添加' + num + '个标签');
         }
     }
-
-    // (4) 删除文章标签
-    // 遍历所有标签列 给x号绑定点击事件
-    function tagsListClickEvent(tagsList) {
-        tagsList = $(".mark-section-title > div:first").children(".tags-list");
-        tagsCount(tagsList);
-        tagsList.each(function (index, domEle) {
-            $(domEle).on("click", ".icon-remove", function () {
-                $(this).parent(".tags-list").remove();
-                tagsList = $(".mark-section-title > div:first").children(".tags-list");
-                // console.log(tagsList);
-                console.log(tagsList.length);
-                tagsCount(tagsList);
-            })
-        })
-    }
-
-    // 获取元素
-    // 分类专栏部分 
-    var classfyArea = $(".new-classfy .flex-box");
-    // 获取分类专栏下面的选项
-    var optionsList = $(".tag-options .tag-options-box>input");
-    // 判断input被选中几个
-    var inputLength = $(".tag-options .tag-options-box>input:checked").length;
-    var newTagText = $(".flex-box .new-classfy-tag span").html();
-    var newClassfyTag = '<div class="add-right new-classfy-tag tag-active"><span>' + newTagText + '</span><i class="icon icon-remove">x</i></div>';
-    console.log(inputLength);
-
-
-    // 分类专栏点击添加新专栏
-    classfyArea.find(".add-new-classfy-tag").on("click", function () {
-        // 先创建一个input 把用户输入的值赋给input 再把input的值给文本 同时删除input
-        var inputValue;
-        inputValue.on("keyup", function (e) {
-
-        })
-        var greatClassfyTag = '<div class="add-right new-classfy-tag tag-active"><input>' + inputValue + '</input><i class="icon icon-remove">x</i></div>'
-    })
-
-
-    // 专栏中的x
-    var closeBtns = $(".new-classfy .icon-remove");
-
-    function closeBtnClickEvent(closeBtns) {
-        closeBtns = $(".new-classfy .icon-remove");
-        inputLength = $(".tag-options .tag-options-box>input:checked").length;
-        // 遍历绑定事件
-        closeBtns.each(function (index, domEle) {
-            $(domEle).on("click", function () {
-                $(this).parent(".new-classfy-tag").remove();
-                closeBtns = $(".new-classfy .icon-remove");
-                // 当前关闭按钮的值
-                var thisText = $(this).siblings("span").text();
-                console.log(thisText + " this");
-                // 关闭按钮对应的input
-                var thisToInput = $(this).parents(".flex-box").siblings(".tag-options").find(".spanValue");
-                // 删除对应的input
-                console.log(closeBtns.length);
-            })
-        })
-        inputLength = $(".tag-options .tag-options-box>input:checked").length;
-    }
-    closeBtnClickEvent(closeBtns);
-
-
-
-    optionsList.each(function (index, domEle) {
-        // $(domEle).eq(0);
-        $(domEle).on("click", function () {
-            console.log($(this).get(0).checked);
-            // 表单被选中 添加选中的分类专栏
-            if ($(this).get(0).checked) {
-                // 小于3可以选择
-                inputLength = $(".tag-options .tag-options-box>input:checked").length;
-                if (inputLength <= 3) {
-                    newTagText = $(this).siblings(".spanValue").html();
-                    newClassfyTag = '<div class="add-right new-classfy-tag tag-active"><span>' + newTagText + '</span><i class="icon icon-remove">x</i></div>';
-                    console.log(newTagText);
-                    $(this).parents(".tag-options").siblings(".flex-box").prepend(newClassfyTag);
-                    inputLength = $(".tag-options .tag-options-box>input:checked").length;
-                    closeBtnClickEvent(closeBtns);
-                } else {
-                    alert("最多只能添加3个分类专栏！");
-                    $(this).get(0).checked = false;
-                }
-
-            } else {
-                // 删除对应的分类专栏
-                // $(this).parents(".tag-options").siblings(".flex-box").before(newClassfyTag).remove();
+    addtag();
+    // 新建分类专栏
+    $(".right .new-class").on("click", function () {
+        var content = $('<button class="add-tag add-tag-class"><span contenteditable="true"></span><i class="add-tag-close">x<i></button>')
+        $(".right .new-class").before(content);
+        classChanged();
+        // $(".right .add-tag").contentEditable = true;
+        $(".right .add-tag span").focus();
+        $(".right .add-tag span").on("keydown", function (e) {
+            if ($(this).html() !== "" && e.keyCode == 13) {
+                this.contentEditable = "false";
             }
         })
-    })
+        // 为新添加的标签X绑定点击事件
+        removeTag($(".add-tag-close"), classChanged);
 
+    })
+    // 添加已有专栏
+    $(".checkbox").change(function (e) {
+        if ($(this).prop("checked")) {
+            var content = $('<button class="add-tag add-tag-class"><span>' + $(this).siblings("span").text() + '</span><i class="add-tag-close">x<i></button>');
+            $(".right .new-class").before(content);
+            classChanged();
+        } else {
+            $(".right .add-tag span").each(function() {
+                if($(this).text() == $(e.target).siblings("span").text()) {
+                    $(this).parent().remove();
+                    classChanged();
+                }
+            })
+        }
+        removeTag($(".add-tag-close"), classChanged);
+    })
+    function classChanged() {
+        if ($(".add-tag-class").length == 3) {
+            $(".right .new-class").hide()
+            $(".max-class-num").show()
+            $(".max-class-num-mask").css('display', 'block')
+        } else {
+            $(".right .new-class").show();
+            $(".max-class-num").hide();
+            $(".max-class-num-mask").css('display', 'none')
+        }
+    }
+    // $(".modal-bottom button").click(function() {
+    //     var tags = [];
+    //     var classify = [];
+    //     $(".mark-selection .add-tag").each(function() {
+    //         console.log($(this).children("span").text());
+    //         tags.push($(this).children("span").text());
+    //     }); //选择的标签
+    //     $(".right .add-tag").each(function() {
+    //         classify.push($(this).children("span").text());
+    //     }); // 选择的分类
+    //     var blogLabel = tags.join(',');
+    //     var blogType = classify.join(',');
+    //     console.log(tags.join(','));
+    //     console.log(classify.join(','));
+    // })
+
+
+    $(function() {
+        var editor = editormd("test-editor", {
+            width: "100%",
+            height: 600,
+            path: "/editormd/lib/", //依赖lib文件夹路径
+            saveHTMLToTextarea : true,
+            // 图片上传
+            imageUpload: true,
+            imageFormats: ["png","bmp","jpg","jpeg","gif"],
+            imageUploadURL: "/uploadEditorImage",
+            onload : function() {
+                console.log('onload', this);
+            }
+        });
+
+        $("#input").click(function () {
+            var tags = [];
+            var classify = [];
+            $(".mark-selection .add-tag").each(function() {
+                console.log($(this).children("span").text());
+                tags.push($(this).children("span").text());
+            }); //选择的标签
+            $(".right .add-tag").each(function() {
+                classify.push($(this).children("span").text());
+            }); // 选择的分类
+            var typeString = tags.join(',');
+            var labelString = classify.join(',');
+            console.log(tags.join(','));
+            console.log(classify.join(','));
+            $.post({
+                url: "/editormd",
+                data: {
+                    title: $("#title").val(),
+                    // typeString: 1,// 此处需要异步获取类型id
+                    // 存入子数据库的是markdown源码editor.getMarkdown()
+                    editormd: editor.getMarkdown(),
+                    typeString: typeString,
+                    labelString: labelString
+                },
+                success: function () {
+                    location.href="/";
+                },
+                error: function () {
+                    console.log("发送失败");
+                    alert("发送失败");
+                }
+            });
+        })
+    });
 })
