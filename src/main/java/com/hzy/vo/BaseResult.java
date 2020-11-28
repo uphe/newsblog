9cf3755704e763b3db303f52cf74f1b21a317703
贺志营 <l>
2020-11-28 21:35:56 +0800

    add BaseResult

---
+++ src/main/java/com/hzy/vo/BaseResult.java
@@ -0,0 +1,91 @@
+package com.hzy.vo;
+
+/**
+ * @Author: hzy
+ * @Date: 2020/11/28
+ */
+public class BaseResult<T> {
+    private static final Integer SUCCESS_CODE = 0;
+
+    private Integer code;
+
+    private String errorMessage;
+
+    private Boolean success;
+
+    private T data;
+
+    public BaseResult() {
+    }
+
+    public BaseResult(Integer code, boolean success) {
+        this.code = code;
+        this.success = success;
+    }
+
+    public BaseResult(Integer code, String msg, boolean success) {
+        this.code = code;
+        this.errorMessage = msg;
+        this.success = success;
+    }
+
+    public BaseResult(Integer code, T data, boolean success) {
+        this.code = code;
+        this.data = data;
+        this.success = success;
+    }
+
+    public BaseResult(Integer code, String msg, T data, boolean success) {
+        this.code = code;
+        this.errorMessage = msg;
+        this.data = data;
+        this.success = success;
+    }
+
+    public static BaseResult ok(Object result) {
+        return new BaseResult(SUCCESS_CODE, result, true);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return errorMessage;
+    }
+
+    public void setMsg(String msg) {
+        this.errorMessage = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BaseResult{" +
+                "code=" + code +
+                ", msg='" + errorMessage + '\'' +
+                ", success=" + success +
+                ", result=" + data +
+                '}';
+    }
+}
